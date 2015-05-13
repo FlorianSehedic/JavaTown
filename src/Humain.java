5d205c952f4dde13f4a2a5df595c1bdfadc29fec
remiAr <remi.arminio@gmail.com>
2015-05-13 17:09:22 +0200

    Modification de la classe humain et ajout de la classe main Histoire

--- src/Humain.java
+++ src/Humain.java
@@ -1,4 +1,18 @@
 
 public class Humain {
-
+	private String nom;
+	private String boisson;
+	
+	public Humain(String unNom, String uneBoisson){
+		nom = unNom;
+		boisson = uneBoisson;
+	}
+	public String quelEstTonNom(){
+		return nom;
+	}
+	
+	public String parle (){
+		return "Bonjour! Je m'appel "+nom+ " et ma boisson préféré est "+boisson;
+	}
+	
 }
