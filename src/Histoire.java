c9d9f4bc7451abc24d776b5063e61f2b99b6224b
remiAr <remi.arminio@gmail.com>
2015-05-13 17:10:42 +0200

    création de la classe Histoire

---
+++ src/Histoire.java
@@ -0,0 +1,9 @@
+
+public class Histoire {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Humain Humain1 = new Humain("Didier","l'eau");
+		System.out.println(Humain1.parle());
+	}
+}
