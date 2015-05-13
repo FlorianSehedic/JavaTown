40c5293b6df388f17a54cf021d86896babbc88e3
Florian.S <floriansehedic@gmail.com>
2015-05-13 17:33:21 +0200

    Création de main Histoire, modificatin de Humain et création de Brigand

---
+++ src/Histoire.java
@@ -0,0 +1,11 @@
+
+public class Histoire {
+
+	public static void main(String[] args) {
+		Humain Humain1 = new Humain("Clint Eastwood", "l'eau");
+		System.out.println(Humain1.parle());
+		
+	}
+
+
+}
--- src/Humain.java
+++ src/Humain.java
@@ -1,4 +1,23 @@
 
 public class Humain {
-
+	private String nom;
+	private String boisson;
+	
+	public Humain(String unNom, String uneBoisson) {
+		nom = unNom;
+		boisson = uneBoisson;
+	}
+	
+	public String parle() {
+		return "Bonjour, je m'appel "+nom+" et ma boisson favorite est "+boisson+".";
+	}
+	
+	public String quelEstTonNom() {
+		return nom;
+	}
+	
+	public String quelEstTaBoisson() {
+		return boisson;
+	}
+	
 }
